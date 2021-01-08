414b801b3079b9338bde43974828cd9e1d0e9025
Anmol Parande <parandea1.7@gmail.com>
2021-01-07 23:05:37 -0600

    Testing copy files to website when repo is updated

---
+++ website-copy.py
@@ -0,0 +1,15 @@
+import yaml
+import sys
+import shutil
+
+def copy_files(files, destination):
+    for pdffile in files:
+        shutil.copy(pdffile['path'], f"{destination}/{pdffile['filename']}")
+
+if __name__ == '__main__':
+    destination = sys.argv[1]
+
+    with open("website-files.yml") as f:
+        files = yaml.safe_load(f)
+
+    copy_files(files, destination)
