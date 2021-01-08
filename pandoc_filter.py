ebbda118ed1cf1e1152385a85a4ddb0bec89c8ca
Anmol Parande <parandea1.7@gmail.com>
2021-01-08 17:06:21 -0600

    Updated image paths

--- pandoc_filter.py
+++ pandoc_filter.py
@@ -45,12 +45,13 @@ def tikz(key, value, format, _):
         [fmt, code] = value
         if fmt == "latex" and re.match("\\\\begin{tikzpicture}", code):
             outfile = get_filename4code("tikz", code)
+            filename = outfile.split("/")[1]
             filetype = get_extension(format, "png", html="png", latex="pdf")
             src = outfile + '.' + filetype
             if not os.path.isfile(src):
                 tikz2image(code, filetype, outfile)
                 sys.stderr.write('Created image ' + src + '\n')
-            return Para([Image(['', [], []], [], [src, ""])])
+            return Para([Image(['', [], []], [], [f".gitbook/assets/{filename}.{filetype}", ""])])
 
 def inline_math(key, value, format, meta):
     """
